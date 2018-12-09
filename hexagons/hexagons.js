0323426e45ad833afeb7e4870c93510552867b04
Timofey Tipishev <tipishev@gmail.com>
2018-12-09 22:51:45 +0100

    start playing with cooordinates

--- hexagons/hexagons.js
+++ hexagons/hexagons.js
@@ -11,6 +11,7 @@ const sqrt = Math.sqrt
 
 // constants
 const HEX_SIZE = 24
+const FENIX_5_SCREEN_RADIUS = 120
 
 // calculated constants
 const HEX_WIDTH = HEX_SIZE * sqrt(3)
@@ -25,30 +26,6 @@ const Point = function (x, y) {
   return { x: x, y: y }
 }
 
-// draw hexagon
-
-const pointyHexCorner = function (center, size, idx) {
-  let angleInDegrees = 60 * idx - 30
-  let angleInRadians = PI / 180 * angleInDegrees
-  return Point(center.x + size * cos(angleInRadians),
-    center.y + size * sin(angleInRadians))
-}
-
-const drawHex = function (ctx, center, size) {
-  print('Drawing a hex')
-  let corners = []
-  for (let i = 0; i < 6; i++) {
-    let corner = pointyHexCorner(center, size, i)
-    corners.push(corner)
-  }
-  ctx.moveTo(corners[0].x, corners[0].y)
-  for (let corner of corners) {
-    ctx.lineTo(corner.x, corner.y)
-  }
-  ctx.lineTo(corners[0].x, corners[0].y)
-  ctx.stroke()
-}
-
 const Board = function (spec) {
   // extract from spec
   // let width = spec.width
@@ -59,8 +36,33 @@ const Board = function (spec) {
 
   const self = {}
 
+  const pointyHexCorner = function (center, size, idx) {
+    let angleInDegrees = 60 * idx - 30
+    let angleInRadians = PI / 180 * angleInDegrees
+    return Point(center.x + size * cos(angleInRadians),
+      center.y + size * sin(angleInRadians))
+  }
+
+  // private
+  const drawHex = function (ctx, center, size) {
+    print('Drawing a hex')
+    let corners = []
+    for (let i = 0; i < 6; i++) {
+      let corner = pointyHexCorner(center, size, i)
+      corners.push(corner)
+    }
+    ctx.moveTo(corners[0].x, corners[0].y)
+    for (let corner of corners) {
+      ctx.lineTo(corner.x, corner.y)
+    }
+    ctx.lineTo(corners[0].x, corners[0].y)
+    ctx.stroke()
+  }
+
+  // public
   self.draw = function () {
     let row = 0
+    let col = 0
     let x = 0
     let y = 0
     while (y < canvas.height) {
@@ -74,7 +76,8 @@ const Board = function (spec) {
     }
   }
 
-  self.drawCircle = function (center, radius) {
+  self.drawBounds = function (center, radius) {
+    radius = radius || FENIX_5_SCREEN_RADIUS
     let x = center.x
     let y = center.y
     ctx.moveTo(x + radius, y)
@@ -85,5 +88,8 @@ const Board = function (spec) {
   return self
 }
 
+// show time
+
 let board = Board({ widht: 1024, height: 780 })
 board.draw()
+board.drawBounds(Point(120, 120))
